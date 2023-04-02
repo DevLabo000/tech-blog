77ee8e947686a19365fed65bad06370455ecb077
DevLabo <monolab000@gmail.com>
2023-04-03 00:17:06 +0900

    詳細ページに画像とタイトルを追加

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -4,7 +4,7 @@
 import { load } from 'cheerio';
 import hljs from 'highlight.js';
 import { AppMeta, Content } from 'newt-client-js';
-import Link from 'next/link';
+import Image from 'next/image';
 import { useEffect } from 'react';
 import tocbot from 'tocbot';
 
@@ -45,12 +45,25 @@ export const ArticlePage = (props: ArticlePageProps) => {
   });
   currentArticle.body = $.html();
 
+  const yyyy = currentArticle._sys.createdAt.substring(0, 4);
+  const mm = currentArticle._sys.createdAt.substring(5, 7);
+  const dd = currentArticle._sys.createdAt.substring(8, 10);
+  const mmdd = `${mm}/${dd}`;
+
   return (
     <Layout app={app} meta={{ description: 'aaaa', ogImage: 'a' }}>
-      <div className="md:flex md:flex-wrap md:justify-center bg-white p-4">
-        <div className="md:w-2/3">
-          <div className="">
-            <h1 className="">{currentArticle.title}</h1>
+      <div className="md:flex md:flex-wrap md:justify-center">
+        <main className="bg-white rounded-md md:w-1/2 p-5 md:p-10">
+          <div className="flex flex-wrap w-auto mb-5">
+            <div className="border-r">
+              <div className="text-gray-500 text-center mr-3">
+                <p className="text-xs">{yyyy}</p>
+                <p className="text-md">{mmdd}</p>
+              </div>
+            </div>
+            <h1 className="px-3 w-auto text-bold text-xl">{currentArticle.title}</h1>
+          </div>
+          {/* 
             <ul className="">
               {currentArticle.tags.map((tag) => (
                 <li key={tag._id}>
@@ -58,73 +71,24 @@ export const ArticlePage = (props: ArticlePageProps) => {
                 </li>
               ))}
             </ul>
-            <div className="">
-              <div className="">
-                <a href="#" className="">
-                  {currentArticle.author?.profileImage?.src ? (
-                    <img src={currentArticle.author.profileImage.src} alt="" width="32" height="32" />
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20px"
-                      height="20px"
-                      viewBox="0 0 24 24"
-                      fill="#CCCCCC"
-                    >
-                      <path d="M0 0h24v24H0V0z" fill="none" />
-                      <path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 10c2.7 0 5.8 1.29 6 2H6c.23-.72 3.31-2 6-2m0-12C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 10c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                    </svg>
-                  )}
-                </a>
-                <div className="">
-                  <div></div>
-                  {/* 
-                <Link href={`/author/${currentArticle.author.slug}`}>
-                  <a className={styles.Article_AuthorName}>{authorName}</a>
-                </Link>
-                */}
-                </div>
-              </div>
-              <div className="">
-                <p className="">Share this post</p>
-                {/*
-              <ul className=''>
-                <li className="mx-2">
-                  
-                  <button type="button" onClick={shareOnTwitter} aria-label="link-twitter">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="24px"
-                      viewBox="0 0 24 24"
-                      width="24px"
-                      fill="#cccccc"
-                    >
-                      <path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
-                    </svg>
-                  </button>
-                </li>
-                <li className="mx-3">
-                  <button type="button" onClick={shareOnFacebook} aria-label="link-facebook">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="24px"
-                      viewBox="0 0 24 24"
-                      width="24px"
-                      fill="#cccccc"
-                    >
-                      <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z" />
-                    </svg>
-                  </button>
-                </li>
-              </ul>
-               */}
-              </div>
-            </div>
+            */}
+          <div className="flex justify-center bg-red-50 rounded-3xl overflow-hidden mb-5">
+            <Image
+              src={currentArticle.coverImage.src}
+              alt="tete"
+              style={{
+                width: '70%',
+                height: 'auto',
+              }}
+              width={1980}
+              height={1150}
+              sizes="100vw"
+            />
           </div>
-          <div className="prose text-black pt-7 " dangerouslySetInnerHTML={{ __html: currentArticle.body }} />
-        </div>
-        <aside className="hidden sm:hidden md:hidden lg:block xl:block">
-          <div className="sticky top-12 bg-white p-4">
+          <div className="prose text-black mt-5" dangerouslySetInnerHTML={{ __html: currentArticle.body }} />
+        </main>
+        <aside className="ml-10 w-1/5 hidden sm:hidden md:hidden lg:block xl:block">
+          <div className="sticky top-12 bg-white p-5 md:p-10">
             <div className="">
               <p className="text-lg pb-3 font-bold ">目次</p>
               <nav className="toc " />
